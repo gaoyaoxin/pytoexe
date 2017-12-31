8ee66921fa07de46e46b9fda2664fbc69b9386fb
Michael Cukier <33379581+michaelcukier@users.noreply.github.com>
2017-12-31 01:37:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -157,58 +157,6 @@ io.on('connection', function(client) {
   });
 });
 
-/*
-app.post('/uploadpy', function(req, res) {
-
-  pyfile = req.files.sampleFile;
-  unique_id = shortid.generate();
-  libs = req.body.py_libraries;
-  pyfilewithoutext = (req.files.sampleFile.name).split(".")[0];
-
-  fs.mkdirSync('./pyfiles/' + unique_id);
-  fs.mkdirSync('./public/pyfilesdownload/' + unique_id);
-
-  // move file to proper dir
-  pyfile.mv('./pyfiles/' + unique_id + "/" + pyfile.name, function(err) {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      // if succcessfully moved file, we can start convertion
-      res.render('convertion', {
-        pyfile: pyfile.name,
-        uniqueid: unique_id,
-        libs: libs
-      });
-      // when user land on the initial convertion page
-      io.on('connection', function(client) {
-        client.on('subscribe', function(data) { client.join(data.room); })
-        console.log('Client connected...');
-
-        // when user click on 'convert'
-        client.on('convertstart', function(data) {
-          console.log('Joined room ' + data);
-          client.join(data);
-
-          // #1 -> download libraries with pip
-          var result = require('child_process').execSync('sudo wine pip3 install ' + libs);
-          io.to(data).emit('step1', libs);
-
-          // #2 -> run pyinstaller
-          var result = require('child_process').execSync('wine pyinstaller -y ./pyfiles/' + unique_id + "/" + pyfile.name + " --distpath ./pyfiles/" + unique_id + "/dist/ --workpath ./pyfiles/" + unique_id + "/build/ --onefile");
-          io.to(data).emit('step2', '');
-
-          // #3 -> move file to public folder
-          var result = require('child_process').execSync('cp ./pyfiles/' + unique_id + "/dist/" + pyfilewithoutext + ".exe ./public/pyfilesdownload/" + unique_id + "/");
-          io.to(data).emit('step3', {pyfilewithoutext: pyfilewithoutext, unique_id:unique_id});
-
-          client.leave(data);
-        });
-      });
-    }
-  });
-});
-*/
-
 app.get('/', function(req, res){
   res.render('makeconv');
 });
